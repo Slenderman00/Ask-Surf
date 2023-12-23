efffc54a8a5acb83224e18977d766106ec430aa3
Joar Heimonen <joarheimonen@live.no>
2023-12-23 17:38:22 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AskSurf',
-    version='0.5.8',
+    version='0.5.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
