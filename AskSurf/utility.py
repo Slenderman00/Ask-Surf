6fe3b8c399dcc50635cdce73814babd36786644e
Joar Heimonen <contact@joar.me>
2024-12-25 18:38:51 +0100

    feat: get console width

--- AskSurf/utility.py
+++ AskSurf/utility.py
@@ -9,7 +9,7 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters import TerminalFormatter
 from pygments.util import ClassNotFound
 from .settings import load_settings
-
+import shutil
 
 def detect_file_type(data):
     """Detect the file type of the data"""
@@ -67,9 +67,15 @@ def handle_code_blocks(message):
     return message
 
 
-def parse_message(message):
+def get_image_width():
+    """Get the width of the console"""
     settings = load_settings()
+    if settings['image']['cli_image_width'] == 0:
+        return shutil.get_terminal_size().columns
+    return settings['image']['cli_image_width']
 
+
+def parse_message(message):
     message = message[1:]
 
     # replace the tags with the correct color codes
@@ -98,7 +104,7 @@ def parse_message(message):
         start_index = message.index("[I]") + len("[I]")
         end_index = message.index("[/I]")
         image_path = message[start_index:end_index]
-        image_str = "\n" + climage.convert(image_path, is_unicode=True, width=settings['image']['cli_image_width'])
+        image_str = "\n" + climage.convert(image_path, is_unicode=True, width=get_image_width())
         message = message[:start_index - len("[I]")] + image_str + message[end_index + len("[/I]"):]
 
     message = handle_code_blocks(message)
